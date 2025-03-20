dc75ffef016fc6d1e3921467956d59b6c39ce537
Igor Vučko <ivucko1@fina.hr>
2025-03-20 14:52:33 +0100

    Updated with lazy

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MoviesContainer from "../features/movies/containers/MoviesContainer";
-import { SingleMovieContainer } from "../features/singles";
+import { Suspense, lazy } from "react";
 
+const MoviesContainer = lazy(() => import('../features/movies/MoviesContainer.tsx'));
+const SingleMovieContainer = lazy(() => import('../features/singles/SingleMovieContainer.tsx'));
 const App = () => {
   return (
     <Router>
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<MoviesContainer />} />
         <Route path="/movies/:id" element={<SingleMovieContainer />} />
       </Routes>
+      </Suspense>
     </Router>
   );
 };
